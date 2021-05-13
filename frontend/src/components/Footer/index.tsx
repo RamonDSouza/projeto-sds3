562af23921d07da1e4fff871d6d397f4818675ae
Ramon Duarte <ramon_duarte.10@hotmail.com>
2021-05-13 17:49:36 -0300

    components add

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+const Footer = ()=>{
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+    <div className="container">
+      <p className="text-light">App desenvolvido por <a href="https://github.com/RamonDSouza" target="_blank" rel="noreferrer">Ramon Duarte</a></p>
+      <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+        Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/ramonduarte100/" target="_blank" rel="noreferrer">@ramonduarte100</a></small></p>
+    </div>
+  </footer>
+  
+  
+  );
+}
+
+export default Footer;
